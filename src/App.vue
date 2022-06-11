1ec4203f463b00c6e17f3429cf32ff45b4ec6f2a
MrEraxd <MrEraxd@gmail.com>
2022-06-11 18:51:43 +0200

    Init commit.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  /// <reference types="vite-svg-loader" />
+
+  import './assets/styles/main.pcss';
+</script>
+
+<template>
+  <div>aaa</div>
+</template>
+
+<style lang="postcss"></style>
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+import svgLoader from 'vite-svg-loader';
+import fs from 'fs';
+import path from 'path/posix';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), eslintPlugin({ cache: false }), svgLoader()],
+  base: './',
+  server: {
+    https: {
+      key: fs.readFileSync('./.cert/localhost-key.pem'),
+      cert: fs.readFileSync('./.cert/localhost.pem'),
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@svg',
+        replacement: path.resolve(path.dirname(''), 'src/assets/svg'),
+      },
+      {
+        find: '@base',
+        replacement: path.resolve(path.dirname(''), 'src/components/Base'),
+      },
+      {
+        find: '@plugins',
+        replacement: path.resolve(path.dirname(''), 'src/plugins'),
+      },
+    ],
+  },
+});
